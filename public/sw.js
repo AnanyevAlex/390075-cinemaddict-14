4ceeabe23534fd40c81c9c8cc30e6012f0d79d59
Alex Ananyev <aaa111@phoenix-cg.ru>
2021-05-20 12:23:07 +0500

    update

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,6 @@
 const CACHE_PREFIX = 'cinemaddict-cache';
 const CACHE_VER = 'v14';
 const CACHE_NAME = `${CACHE_PREFIX}-${CACHE_VER}`;
-
 const HTTP_STATUS_OK = 200;
 const RESPONSE_SAFE_TYPE = 'basic';
 
@@ -53,7 +52,6 @@ self.addEventListener('activate', (evt) => {
               if (key.startsWith(CACHE_PREFIX) && key !== CACHE_NAME) {
                 return caches.delete(key);
               }
-
               return null;
             })
             .filter((key) => key !== null),
@@ -64,25 +62,20 @@ self.addEventListener('activate', (evt) => {
 
 const handleFetch = (evt) => {
   const {request} = evt;
-
   evt.respondWith(
     caches.match(request)
       .then((cacheResponse) => {
         if (cacheResponse) {
           return cacheResponse;
         }
-
         return fetch(request)
           .then((response) => {
             if (!response || response.status !== HTTP_STATUS_OK || response.type !== RESPONSE_SAFE_TYPE) {
               return response;
             }
-
             const clonedResponse = response.clone();
-
             caches.open(CACHE_NAME)
               .then((cache) => cache.put(request, clonedResponse));
-
             return response;
           });
       }),
